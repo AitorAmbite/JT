18f88a0442d9481022baf2d2ed4ca37cc4eabaf0
aitor <aitorambite@protonmail.com>
2020-06-13 23:43:52 +0200

    Updates

---
+++ precache-manifest.57bc9c7dd3c4642cf6e5b04b53934bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0172a0224342c88aa66ee1bad3b90b2b",
+    "url": "/JT/index.html"
+  },
+  {
+    "revision": "e10b223c77b21a54e1f0",
+    "url": "/JT/static/css/main.0fd3c67f.chunk.css"
+  },
+  {
+    "revision": "2d09f3f68de8be009202",
+    "url": "/JT/static/js/2.2e08275b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JT/static/js/2.2e08275b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10b223c77b21a54e1f0",
+    "url": "/JT/static/js/main.086a8635.chunk.js"
+  },
+  {
+    "revision": "40b47b8baa6dd7a5895c",
+    "url": "/JT/static/js/runtime-main.d1cef0f1.js"
+  }
+]);
